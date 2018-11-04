b06b686b617017d0891c39e94014d5e8ed5b1f13
OsamaHaroon53 <sonijaosama@gmail.com>
2018-11-04 22:22:37 +0500

    Add select module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ export class AppComponent {
   onchange($event){
     console.log($event);
   }
+  value="1";
+  foods = [
+    {id: '0', viewValue: 'Steak'},
+    {id: '1', viewValue: 'Pizza'},
+    {id: '2', viewValue: 'Tacos'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,9 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatCheckboxModule,
-    MatRadioModule
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
